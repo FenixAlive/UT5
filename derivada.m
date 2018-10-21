1ad7a1b7c128d8e94d8f8a7c501930feeaadce9d
AngelLegion <luisangelmf@gmail.com>
2018-10-21 01:20:54 -0500

    agregue formas de integral de rectangulos

--- derivada.m
+++ derivada.m
@@ -1,14 +1,17 @@
-f = @(x)(1./(1+x.^2));
-fp1 = @(x,h)((f(x+h)-f(x))./h);
-fp2 = @(x,h)((f(x+h)-f(x-h))./(2.*h));
-n = 8;
-x = 1
+%Solo se debe modificar f, n, x y h
+% f = @(x)(1./(1+x.^2)); %función a derivar
+f = @(x)(exp((log(x)-x.^3)./(3*x.^2-cos(5*x.^7))));
+fp1 = @(x,h)((f(x+h)-f(x))./h); %primera formula para derivar la de toda la vida
+fp2 = @(x,h)((f(x+h)-f(x-h))./(2.*h)); %segunda formula basada en la serie de taylor
+
+n = 10; % 10^-n
+x = 3; %punto de calculo de derivada
 h = 10.^(-(1:n));
 % matriz con resultados  
-[h' fp1(x,h)' fp2(x,h)']
-
-d2f = @(x,h)((f(x+h)-2.*f(x)+f(x-h))./(2.*h.^2));
-[h' fp1(x,h)' fp2(x,h)' d2f(x,h)']
+rf = [h' fp1(x,h)' fp2(x,h)']
+%formula para la segunda derivada basada en taylor
+d2f = @(x,h)((f(x+h)-2*f(x)+f(x-h))./(h.^2));
+r2f = [h' fp1(x,h)' fp2(x,h)' d2f(x,h)']
 
 
 
--- integral.m
+++ integral.m
@@ -1,9 +1,31 @@
 clear
-k = 4;
+k = 50;
 a = -1;
 b = 1;
 h = (b-a)/(k);
-x = a+h*(0:(k-1));
 
+%hay tres formas de calcular la integral, elegir c = ?
+%1-rectangulos interiores
+%2-rectangulos exteriores
+%3-rectangulos intermedios
+
+c = 2
+if c == 1
+% x para rectangulos interiores
+    x = a+h*(0:(k-1));
+elseif c == 2
+% x para rectangulos exteriores
+    x = a+h*(1:k);
+elseif c == 3
+%x para rectangulos intermedios
+    xt = a+h*(1:k+1);
+    x = [];
+    for i = 1:k
+        x(i) = (xt(i+1)-xt(i))/2;
+    end
+end
+    
+%función a integrar
 f = @(x)(4-x.^2);
-integral = sum(f(x)*h)
+%formula de integración
+in = sum(f(x)*h)
