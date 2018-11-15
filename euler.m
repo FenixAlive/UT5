963fe64474ff3b4f5c4e799a94b036210603a976
FenixAlive <luisangelmf@gmail.com>
2018-11-15 15:54:41 -0600

    ver si se necesita tn

--- euler.m
+++ euler.m
@@ -1,17 +1,26 @@
 clear
 %f = @(x,t)(exp(t));
-f = @(x,y)(y-(x.^3-x.^2+x).*exp(-x.^2));
+%f = @(x,y)(y-(x.^3-x.^2+x).*exp(-x.^2));
+f = @(x,y)((9*x.^2-2).*cos(3*x.^3-2*x));
+fexac = @(x)(sin(3.*x.^3-2*x));
 a = 0; %limite inferior
-b = 3; %limite superior
-h = .1; %Separacion
+b = 7; %limite superior
+h = 10^-2; %Separacion
 t0 = 0;
-y0 = 1;
-hl = a:h:b; %espacio lineal
+y0 = 0;
+h1 = a:h:b; %espacio lineal
 yv = y0 + h * f(t0+a,y0);
 yn = yv;
-for n = 2:length(hl)
-    tn = t0+hl(n);
+tf = t0;
+for n = 2:length(h1)
+    tn = t0+h1(n); %arreglar el tn, creo que no se usa t0, o creo que  es lo mismo que hl
+    tf = [tf tn];
     yv = yv + h * f(tn,yv);
     yn = [yn yv];
 end
-plot(hl, yn, 'b:')
+figure(1);
+plot(h1, yn, 'b', h1, fexac(h1), 'g');
+figure(2);
+plot(abs(yn-fexac(h1)))
+figure(3);
+plot(tf, fexac(tf), 'r:')
--- integral.m
+++ integral.m
@@ -7,27 +7,14 @@ h = (b-a)/(k);
 %función a integrar
 f = @(x)(x.^3-11.*x.^2./6+x+11/6);
 %f = @(x)(4-x.^2);
-%hay tres formas de calcular la integral, elegir c = ?
-%1-rectangulos interiores
-%2-rectangulos exteriores
-%3-rectangulos intermedios
 
-c = 3
-if c == 1
 % x para rectangulos interiores
-    x = a+h*(0:(k-1));
-elseif c == 2
+    x1 = a+h*(0:(k-1));
+    in1 = sum(f(x1)*h)
 % x para rectangulos exteriores
-    x = a+h*(1:k);
-elseif c == 3
-%x para rectangulos intermedios
-    %xt = a+h*(1:k+1);
-    %x = [];
-    %for i = 1:k
-    %    x(i) = (xt(i+1)-xt(i))/2;
-    %end
-    x = a+h*(1:k)-h/2;
-end
+    x2 = a+h*(1:k);
+    in2 = sum(f(x2)*h)
+% x para rectangulos intermedios
+    x3 = a+h*(1:k)-h/2;
+    in3 = sum(f(x3)*h)
     
-%formula de integración
-in = sum(f(x)*h)
